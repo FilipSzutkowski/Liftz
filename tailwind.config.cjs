992777eaca427fd69347981c43defaaa0c6b2647
FilipSzutkowski <filip99.ski@gmail.com>
2021-10-16 21:33:18 +0200

    tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  purge: ['./index.html', './src/**/*.{svelte,js,ts}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
